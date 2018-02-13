4c9d47e6df115ff2488d9dd8968bb53fb6e2a0b6
Gregory Brown <gbrown07wk@gmail.com>
2018-02-12 20:37:13 -0500

    routing work

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'fake' data
+var friendList = [
+  {
+    name: 'Gregory Q. Brown',
+        profilePic: 'https://avatars3.githubusercontent.com/u/31443727?s=400&u=60678b75a33920ac18f480187c58811154915b9e&v=4'
+        scores[
+
+    "5",
+    "2",
+    "4",
+    "5",
+    "4",
+    "4",
+    "3",
+    "4"
+    ]
+  }
+]
+
+module.exports = friendList;
